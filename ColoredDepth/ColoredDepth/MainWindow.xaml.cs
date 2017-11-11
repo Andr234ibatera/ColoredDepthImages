88573a96a895aca4fae56f99dbbc39f886f6b8c3
Andre Luiz <andr234ibatera@gmail.com>
2017-11-11 16:30:06 -0400

    Depth Image
    Getting depth and color frame

--- ColoredDepth/ColoredDepth/MainWindow.xaml.cs
+++ ColoredDepth/ColoredDepth/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace ColoredDepth
 
         KinectSensor kinectSensor = KinectSensor.GetDefault();
 
+        //Color Parameters
         FrameDescription colorFrameDescription;
         ColorFrameReader colorFrameReader;
         WriteableBitmap colorBitmap;
@@ -32,6 +33,17 @@ namespace ColoredDepth
         Int32Rect colorRect;
         byte[] colorBuffer;
         ColorImageFormat colorImageFormat = ColorImageFormat.Bgra;
+
+        //Depth Parameters
+        DepthFrameReader depthFrameReader;
+        FrameDescription depthFrameDescription;
+        WriteableBitmap depthImage;
+        ushort[] depthBuffer;
+        byte[] depthBitmapBuffer;
+        Int32Rect depthRect;
+        int depthStride;
+        Point depthPoint;
+        const int R = 20;
 
 
         public MainWindow()
@@ -53,6 +65,7 @@ namespace ColoredDepth
                     kinectSensor.Open();
                     LblStatus.Content = "Kinect Connect";
 
+                    //Color manipulation
                     colorFrameDescription = kinectSensor.ColorFrameSource.CreateFrameDescription(colorImageFormat);
 
                     colorFrameReader = kinectSensor.ColorFrameSource.OpenReader();
@@ -63,6 +76,22 @@ namespace ColoredDepth
                     colorRect = new Int32Rect(0, 0, colorFrameDescription.Width, colorFrameDescription.Height);
                     colorBuffer = new byte[colorStride * colorFrameDescription.Height];
                     ScreenColor.Source = colorBitmap;
+
+                    //Depth manipulation
+                    depthFrameDescription = kinectSensor.DepthFrameSource.FrameDescription;
+
+                    depthImage = new WriteableBitmap(depthFrameDescription.Width, depthFrameDescription.Height, 96, 96, PixelFormats.Gray8, null);
+                    depthBuffer = new ushort[depthFrameDescription.LengthInPixels];
+                    depthBitmapBuffer = new byte[depthFrameDescription.LengthInPixels];
+                    depthRect = new Int32Rect(0, 0, depthFrameDescription.Width, depthFrameDescription.Height);
+                    depthStride = (int)depthFrameDescription.Width;
+
+                    ScreenDepth.Source = depthImage;
+
+                    depthPoint = new Point(depthFrameDescription.Width / 2, depthFrameDescription.Height / 2);
+
+                    depthFrameReader = kinectSensor.DepthFrameSource.OpenReader();
+                    depthFrameReader.FrameArrived += depthFrameReader_FrameArrived;
                 }
             }
             catch (Exception exception)
@@ -84,7 +113,32 @@ namespace ColoredDepth
             }
             colorBitmap.WritePixels(colorRect, colorBuffer, colorStride, 0);
         }
-        
+
+        private void depthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            try
+            {
+                using (var depthFrame = e.FrameReference.AcquireFrame())
+                {
+                    if (depthFrame == null)
+                    {
+                        return;
+                    }
+                    depthFrame.CopyFrameDataToArray(depthBuffer);
+                }
+                for (int i = 0; i < depthBuffer.Length; i++)
+                {
+                    depthBitmapBuffer[i] = (byte)(depthBuffer[i] % 255);
+                }
+                depthImage.WritePixels(depthRect, depthBitmapBuffer, depthStride, 0);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Close();
+            }
+        }
+
         private void UpdateColorFrame(ColorFrameArrivedEventArgs e)
         {
             throw new NotImplementedException();
